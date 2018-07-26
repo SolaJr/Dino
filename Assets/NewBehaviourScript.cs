da0ca1db2850e974826d0262e40d6e52b633b268
SolaJr <SolaJr@users.noreply.github.com>
2018-07-26 12:53:58 +0800

    initial
    initia

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Spani : MonoBehaviour {
+    public Vector3 center;
+    public Vector3 size;
+    public GameObject Foodprefab; //before it is made
+    //Rotation
+    
+    // Use this for initialization
+    void Start() {
+        SpawnFood(); 
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.Q))
+        { SpawnFood(); }
+    }
+
+    public void SpawnFood()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(20*(-size.x), 20*size.x), Random.Range((-20)*size.y , 20*size.y ));
+
+        Instantiate(Foodprefab, pos, Quaternion.identity);
+        transform.Rotate(0, 90, 0);
+    }
+        
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1,0,0,.5f);
+        Gizmos.DrawCube(center, size);
+      }
+}
---
+++ Assets/menu_buttons.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu_buttons : MonoBehaviour {
+
+    public void GoToMainMenu()
+    {
+        Application.LoadLevel("main_menu");
+    }
+
+    public void GoToARCamera()
+    {
+        Application.LoadLevel("ARcamera");
+    }
+
+    public void GoToFound()
+    {
+        Application.LoadLevel("found");
+    }
+
+    public void GoToArena()
+    {
+        Application.LoadLevel("arena");
+    }
+
+    public void GoToMarket()
+    {
+        Application.LoadLevel("market");
+    }
+
+    public void GoToFossils()
+    {
+        Application.LoadLevel("fossils");
+    }
+
+    public void GoToLab()
+    {
+        Application.LoadLevel("lab");
+    }
+
+    public void GoToSettings()
+    {
+        Application.LoadLevel("settings");
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+}
